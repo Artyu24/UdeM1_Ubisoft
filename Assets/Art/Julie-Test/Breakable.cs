58662762b3fd0efaaea9af2afb359eff8f673915
JuliePec <julie.pecuch@umontreal.ca>
2025-02-20 16:18:25 -0500

    [ADD] Testing breakable mug

---
+++ Assets/Art/Julie-Test/Breakable.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[SelectionBase]
+public class Breakable : MonoBehaviour
+{
+    [SerializeField] GameObject intact;
+    [SerializeField] GameObject broken;
+
+    BoxCollider bc;
+
+    private void Awake()
+    {
+        intact.SetActive(true);
+        broken.SetActive(false);
+
+        bc = GetComponent<BoxCollider>();
+    }
+
+    private void OnMouseDown()
+    {
+        Break();
+    }
+
+    private void Break()
+    {
+        intact.SetActive(false);
+        broken.SetActive(true);
+
+        bc.enabled = false;
+    }
+}
