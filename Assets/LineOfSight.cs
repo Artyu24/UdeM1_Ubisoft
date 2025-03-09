49562d9c5e29464b46a85fdfad4adccc252ad615
Thoma-fr <thomas.raulin.pro@gmail.com>
2025-03-09 11:46:38 -0400

    [WIP] push player

--- Assets/LineOfSight.cs
+++ Assets/LineOfSight.cs
@@ -6,7 +6,10 @@ public class LineOfSight : MonoBehaviour
 {
     [SerializeField] private LayerMask viewMask;
     [SerializeField] private float _reactionTime = 1f;
+    [SerializeField] private float _playerDistanceBeforePush = 1f;
     [field: SerializeField] public List<AIObject> InSight { get; set; }
+    
+    private Coroutine _pushPlayer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,17 +24,14 @@ public class LineOfSight : MonoBehaviour
     }
     private void OnTriggerStay(Collider other)
     {
-        RaycastHit hit;
-        Physics.Raycast(transform.parent.position, (other.transform.position - transform.parent.position).normalized, out hit, 100, viewMask);
-        Debug.DrawRay(transform.parent.position, (other.transform.position - transform.parent.position).normalized * hit.distance, Color.yellow);
-        AddToSighList(hit.collider);
+        if(!SightCheck(other)) return;
+        AddToSighList(other);
+        CheckIsPlayer(other.gameObject);
     }
     private void OnTriggerEnter(Collider other)
     {
-        RaycastHit hit;
-        Physics.Raycast(transform.parent.position, (other.transform.position - transform.parent.position).normalized,out hit, 100, viewMask);
-        Debug.DrawRay(transform.parent.position, (other.transform.position - transform.parent.position).normalized * hit.distance, Color.yellow);
-        AddToSighList(hit.collider);
+        if (!SightCheck(other)) return;
+        AddToSighList(other);
     }
     private void OnTriggerExit(Collider other)
     {
@@ -61,4 +61,31 @@ public class LineOfSight : MonoBehaviour
         }
         return null;
     }
+    private bool SightCheck(Collider other)
+    {
+        RaycastHit hit;
+        Physics.Raycast(transform.parent.position, (other.transform.position - transform.parent.position).normalized, out hit, 100, viewMask);
+        Debug.DrawRay(transform.parent.position, (other.transform.position - transform.parent.position).normalized * hit.distance, Color.yellow);
+        return hit.collider;
+    }
+    private void CheckIsPlayer(GameObject go)//method a refacto pour plus solide
+    {
+        if (go == null) return;
+        Debug.Log((Vector3.Distance(go.transform.position, transform.parent.position)));
+        if (Vector3.Distance(go.transform.position, transform.parent.position) > _playerDistanceBeforePush) return;
+        if(go.TryGetComponent<PlayerMovement>(out PlayerMovement playerMovement))
+        {
+            //playerMovement.OnIAPush(transform.parent.position);
+            if (_pushPlayer != null) return;
+            _pushPlayer=StartCoroutine(PushPlayer(playerMovement));
+        }
+    }
+    private IEnumerator PushPlayer(PlayerMovement playerMovement)
+    {
+        playerMovement.OnIAPush(transform.parent.position);
+        yield return new WaitForSeconds(1f);
+        _pushPlayer=null;
+
+    }
+
 }
