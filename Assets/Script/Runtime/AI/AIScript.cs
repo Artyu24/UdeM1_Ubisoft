1c3b560a0b30463b3e657a752c8fec93c719f378
Thoma-fr <thomas.raulin.pro@gmail.com>
2025-03-24 20:38:23 -0400

    [done] garde ai

--- Assets/Script/Runtime/AI/AIScript.cs
+++ Assets/Script/Runtime/AI/AIScript.cs
@@ -12,7 +12,12 @@ public enum npcState
 {
     wandering,
     chasing,
-    runningAway
+    Reacting,
+    Stunned,
+    leaving,
+    HoldPlayer,
+    LookingArround,
+    yeetPlayer
 }
 public enum npcType
 {
@@ -48,7 +53,7 @@ public class AIScript : MonoBehaviour, IInteractable
     [SerializeField, Required] private NavMeshAgent _agent;
     public LineOfSight LineOfSight { get; set; }//auto ref
 
-    public npcState State { get; protected set; }
+    [field:SerializeField]public npcState State { get; protected set; }
 
     [Header("Player Reactions")]
     [SerializeField] ReactionToplayer _reactionToPlayer;
@@ -56,11 +61,17 @@ public class AIScript : MonoBehaviour, IInteractable
     Vector3 _lastPlayerSeenPosition = Vector3.zero;
     [field:SerializeField]public List<GameObject> interest { get; set; }
     private Coroutine _reactionCoroutine;
-    private Sequence sequence = DOTween.Sequence();
+    private Coroutine _lookingArroundCorou;
+
+    [Header("Player Reactions")]
+    private PlayerMovement _currentChasedPlayer;
 
     [Header("grab")]
     [SerializeField] Transform _grabPosition;
+    [SerializeField] float _StunTime = 3f;
+    [SerializeField] Transform _DropPosition;
     private PlayerMovement _grabbedPlayer;
+    bool _isGnoringPlayer = false;
 
     public UnityEvent Onflee;
     public UnityEvent OnReachDestination;
@@ -73,46 +84,95 @@ public class AIScript : MonoBehaviour, IInteractable
     void Start()
     {
         Wander();
-
+        _agent.destination = _wanderPoints[Random.Range(0,_wanderPoints.Count-1)].position;
         AIEventHandler.instance.Ai.Add(this);
     }
     void Update()
     {
-        if (!_agent.pathPending)//wandering loop, [refacto possible]
+        if (!_agent.pathPending)//check is succes to go to destination
         {
             if (_agent.remainingDistance <= _agent.stoppingDistance)
             {
                 if (!_agent.hasPath || _agent.velocity.sqrMagnitude == 0f)
                 {
-                    if (!_isRunningAway && _wandererDelay==null)
-                    {
-                        OnReachDestination.Invoke();
-                        transform.DORotate(_guardRotation,0.5f);
-                        _wandererDelay = StartCoroutine(DelayBeforeWandering());
-                    }
-                    else if (_isRunningAway)
-                    {
-                        OnReachReactionDestination.Invoke();
-                        LookForObject(_objectType);
-                        
-                    }
+                    ReachAIDestination();
                 }
             }
+        }
+
+    }
+    void LateUpdate()
+    {
+        StateUpdate();
+    }
+    private void StateUpdate()
+    {
+        switch (State)
+        {
+            case npcState.chasing:
+                ChasePlayer(_currentChasedPlayer);
+                break;
+            
+
+            default:
+                break;
+        }
+    }
+    private void ReachAIDestination()
+    {
+        switch (State)
+        {
+            case npcState.wandering:
+                if (_wandererDelay == null)//no
+                {
+                    OnReachDestination.Invoke();
+                    transform.DORotate(_guardRotation, 0.5f);
+                    _wandererDelay = StartCoroutine(DelayBeforeWandering());
+                }
+                break;
+            case npcState.chasing:
+                if (_lookingArroundCorou == null)
+                    _lookingArroundCorou = StartCoroutine(LookArround());
+                break;
+            case npcState.LookingArround:
+                break;
+
+            case npcState.Reacting:
+                OnReachReactionDestination.Invoke();
+                LookForObject(_objectType);
+                break;
+            case npcState.Stunned:
+                break;
+            case npcState.leaving:
+                break;
+            case npcState.HoldPlayer:
+                GoToTrash();
+                break;
+            case npcState.yeetPlayer:
+                EjectPLayer();
+                StartCoroutine(DelayBeforeWandering());
+                break;
+            default:
+                break;
         }
     }
     [Button]
     public void fleeToPoint()
     {
         _agent.destination = _reactionZone.position;
+        State= npcState.leaving;
         _isRunningAway=true;
-        StopAllCoroutines();
+        StopAllCoroutines();// kill all action running
     }
 
     public void LookForObject(string objectType)
     {
         AIObject aIObject = _lineOfSight.GetSightObjectByType(objectType);
         if (aIObject == null)
+        {
+            State = npcState.leaving;
             LeaveZone();
+        }
         else
             UseObject(aIObject);
     }
@@ -130,20 +190,19 @@ public class AIScript : MonoBehaviour, IInteractable
     {
         _agent.destination = _leavePoint.position;
     }
+    public void GoToTrash()
+    {
+        _agent.destination = _DropPosition.position;
+        State = npcState.yeetPlayer;
+    }
     public void Wander()
     {
-        _agent.destination = _wanderPoints[Random.Range(0,_wanderPoints.Count-1)].position;
-        
-    }
-    IEnumerator DelayBeforeWandering()
-    {
-        yield return new WaitForSeconds(WanderCoolDownTime);
-        Wander();
-        _wandererDelay=null;
+        _agent.destination = _wanderPoints[Random.Range(0,_wanderPoints.Count-1)].position; 
     }
 
     public void ReactoPlayer(PlayerMovement player)
     {
+        if(State == npcState.HoldPlayer || State == npcState.chasing|| _isGnoringPlayer) return;
         if (player == null) return;
         if (_reactionCoroutine != null) return;//a move
         switch (_reactionToPlayer)
@@ -152,12 +211,99 @@ public class AIScript : MonoBehaviour, IInteractable
                 _reactionCoroutine = StartCoroutine(PushPlayer(player));
                 break;
             case ReactionToplayer.Chase:
-                sequence.Kill();
-                _reactionCoroutine = StartCoroutine(ChaseStates(player));
+                InitChase(player);
+                //_reactionCoroutine = StartCoroutine(ChaseStates(player));
                 break;
             default:
                 break;
         }
+    }
+
+    private void ChasePlayer(PlayerMovement playerMovement)
+    {
+        _agent.SetDestination(_lastPlayerSeenPosition);
+        if (LineOfSight.PlayerInSight.Contains(playerMovement))
+        {
+            _lastPlayerSeenPosition = playerMovement.transform.position;
+            
+        }
+
+    }
+    private void InitChase(PlayerMovement playerMovement)
+    {
+        State = npcState.chasing;
+        if (_lookingArroundCorou != null)
+        {
+            InterruptCorouAction(_lookingArroundCorou);
+        }
+        _currentChasedPlayer = playerMovement;
+        _lastPlayerSeenPosition = playerMovement.transform.position;
+        ChasePlayer(playerMovement);
+    }
+    private void InterruptCorouAction(Coroutine corouAction) 
+    {
+        if (corouAction == null) return;
+        StopCoroutine(corouAction);
+        corouAction=null;
+    }
+    private IEnumerator LookArround()
+    {
+        
+        State= npcState.LookingArround;
+        transform.DORotate(new Vector3(transform.rotation.x, transform.rotation.y+ 90, transform.rotation.z), 0.5f);
+        yield return new WaitForSeconds(1f);
+        transform.DORotate(new Vector3(transform.rotation.x, transform.rotation.y - 90, transform.rotation.z), 0.5f);
+        yield return new WaitForSeconds(1f);;
+
+        
+        State = npcState.wandering;
+        _lookingArroundCorou = null;
+
+        Wander();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.TryGetComponent<PlayerMovement>(out PlayerMovement playerMovement)&& _grabbedPlayer==null)
+        {
+            if(State!=npcState.chasing) return;
+            _grabbedPlayer = playerMovement;
+            playerMovement.SetIsGrabed(true);
+            playerMovement.transform.position= _grabPosition.position;
+            playerMovement.gameObject.transform.SetParent(transform);
+            State = npcState.HoldPlayer;
+            GoToTrash();
+                  }
+    }
+    [Button]
+    public void Interact()
+    {
+        OnHited.Invoke();
+        if (_grabbedPlayer != null)
+        {
+            EjectPLayer();
+            StartCoroutine(StunnedDelay());
+        }
+    }
+    private void EjectPLayer()
+    {
+        if (_grabbedPlayer != null)
+        {
+            StartCoroutine(IgnorePlayer());
+            _grabbedPlayer.SetIsGrabed(false);
+            _grabbedPlayer.transform.parent = null;
+            _grabbedPlayer.GetComponent<Rigidbody>().AddForce(transform.forward * 10, ForceMode.Impulse);
+            DontDestroyOnLoad(_grabbedPlayer);
+            _grabbedPlayer = null;
+            if(State==npcState.Stunned) return;
+            State = npcState.wandering;
+        }
+    }
+    private IEnumerator StunnedDelay()
+    {
+        State= npcState.Stunned;
+        yield return new WaitForSeconds(_StunTime);
+        State = npcState.wandering;
     }
     private IEnumerator PushPlayer(PlayerMovement playerMovement)
     {
@@ -166,67 +312,16 @@ public class AIScript : MonoBehaviour, IInteractable
         _reactionCoroutine = null;
 
     }
-    private void ChasePlayer(PlayerMovement playerMovement)
+    IEnumerator DelayBeforeWandering()
     {
-
+        yield return new WaitForSeconds(WanderCoolDownTime);
+        Wander();
+        _wandererDelay = null;
     }
-    private IEnumerator ChaseStates(PlayerMovement playerMovement)
+    IEnumerator IgnorePlayer()
     {
-        State = npcState.chasing;
-        Debug.Log("start chase");
-        bool Chasing = true;
-        _lastPlayerSeenPosition = playerMovement.transform.position;
-        while (Chasing) 
-        {
-            _agent.SetDestination(_lastPlayerSeenPosition);
-            if (LineOfSight.PlayerInSight.Contains(playerMovement))
-            {
-                _lastPlayerSeenPosition = playerMovement.transform.position;
-                yield return null;
-
-                //if reach player
-                //catch it and bring him outside the the area by throwing it
-            }
-            else 
-            {
-
-                Chasing = false;
-                LookArround();
-
-            }
-        }
-        _reactionCoroutine= null;
-    }
-    private void LookArround()
-    {
-        Sequence sequence = DOTween.Sequence();
-        sequence.Append(transform.DORotate(new Vector3(transform.rotation.x, transform.rotation.y+ 90, transform.rotation.z), 0.5f));
-        sequence.AppendInterval(1f);
-        sequence.Append(transform.DORotate(new Vector3(transform.rotation.x, transform.rotation.y - 90, transform.rotation.z), 0.5f));
-        sequence.AppendInterval(1f).OnComplete(() => { Debug.Log("comp"); Wander(); }); // a changer!
+        _isGnoringPlayer = true;
+        yield return new WaitForSeconds(1);
     }
 
-    private void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.TryGetComponent<PlayerMovement>(out PlayerMovement playerMovement)&& _grabbedPlayer==null)
-        {
-            _grabbedPlayer = playerMovement;
-            playerMovement.SetIsGrabed(true);
-            playerMovement.transform.position= _grabPosition.position;
-            playerMovement.gameObject.transform.SetParent(transform);
-            
-        }
-    }
-    [Button]
-    public void Interact()
-    {
-        OnHited.Invoke();
-        if (_grabbedPlayer != null) 
-        {
-            _grabbedPlayer.SetIsGrabed(false);
-            _grabbedPlayer.transform.parent=null;
-
-            _grabbedPlayer.GetComponent<Rigidbody>().AddForce(transform.forward * 10, ForceMode.Impulse);
-        }
-    }
 }
