e3474b4adb7f1ba6c58b358239e4722c23022198
Artyu24 <65313164+Artyu24@users.noreply.github.com>
2025-03-31 00:17:36 -0400

    [UM-69][ADD] First Player Anim

--- Assets/Script/Runtime/Player/PlayerData.cs
+++ Assets/Script/Runtime/Player/PlayerData.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 
 public class PlayerData : MonoBehaviour
 {
+    [Header("Components")] 
+    [SerializeField] private Animator _anim;
+    public Animator AnimController => _anim;
+    
     [Header("Data")]
     private int _index;
-    
+
+
     public bool SetupPlayerData(int index)
     {
         _index = index;
