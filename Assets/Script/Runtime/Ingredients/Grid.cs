b636b320f5246ff9b6c2203443e2762a6c6984dd
Kerwan0916 <jkevin.fuss@gmail.com>
2025-04-04 18:34:36 -0400

    [MOD] Grids are now working for lvl 2 and 3 when adding objects to the throne

--- Assets/Script/Runtime/Ingredients/Grid.cs
+++ Assets/Script/Runtime/Ingredients/Grid.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-    public int requiredObjectCount = 0;
+    public int requiredObjectCount = 1;
     private bool isOpen = false;
 
     private void OnEnable()
@@ -26,10 +26,11 @@ public class Grid : MonoBehaviour
     void Open()
     {
         Debug.Log(gameObject.name + " is opening!");
-        gameObject.SetActive(false);
         isOpen = true;
 
         Vector3 direction = new Vector3(0, -1, 0);
         transform.Translate(direction);
+
+        gameObject.SetActive(false);
     }
 }
