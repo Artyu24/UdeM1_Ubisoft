547644510e2dfcdb7a9d96bd922b7c6b42bab36e
Artyu24 <65313164+Artyu24@users.noreply.github.com>
2025-04-06 00:20:35 -0400

    [UP] Throne Interaction

--- Assets/Script/Runtime/Player/PlayerInteraction.cs
+++ Assets/Script/Runtime/Player/PlayerInteraction.cs
@@ -78,6 +78,9 @@ public class PlayerInteraction : MonoBehaviour
 
     public void ReleaseObject()
     {
+        if(_grabbedObj == null)
+            return;
+        
         _grabbedObj.OnRelease();
 
         if (_hasRightObjectInHand)
