7852283402d6c595b6d38e262ac5a80982d0495a
Hilary R <royh11@uqat.ca>
2025-04-10 22:25:25 -0400

    Ajout de sfx

--- Assets/Script/Runtime/Music/StateManager.cs
+++ Assets/Script/Runtime/Music/StateManager.cs
@@ -19,10 +19,17 @@ public enum SoundState
 
     SFX_HUMAN_FOOTSTEPS, // Only on client IA, need to try
     SFX_HUMAN_GRUNT_HURT,
+    SFX_HUMAN_SLIP_ON_WATER,
+    SFX_HUMAN_THROWRACOON_TRASH,
+    SFX_HUMAN_VOICES,
+    SFX_HUMAN_ALARMED,
 
     SFX_RACOON_IDLE, //need to try
     SFX_RACOON_WALK, //need to try
     SFX_RACOON_HIT, //Hit != Interaction or Same ? -> Both for the moment
+
+    SFX_WATERPUDDLE_APPEAR,
+    SFX_MIXWATER_AND_OIL,
+    SFX_TRANSITION_PIPEWALKING
     
-    MUSIC
 }
